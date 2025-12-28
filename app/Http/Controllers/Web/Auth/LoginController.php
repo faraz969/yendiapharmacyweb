f0899c0cf0149562f4522ce8302e6befce5ad363
faraz969 <faraz.anwer6@gmail.com>
2025-12-28 13:48:23 +0500

    fixed login issue

--- app/Http/Controllers/Web/Auth/LoginController.php
+++ app/Http/Controllers/Web/Auth/LoginController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Web\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
